87f4908e322b54d3a0d65ad85ec77e853a5c9568
Sergei Plekhov <sergei.plekhov@dgoi.ru>
2025-06-30 00:55:29 +0300

    flake8 recommendation fix v5

--- 04-deployment/tests/test_predict.py
+++ 04-deployment/tests/test_predict.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 # from .. import predict # Пример импорта вашей логики
 
+
 def test_prediction_logic():
     """
     Проверяет, что логика прогнозирования работает на тестовых данных.
@@ -12,15 +13,20 @@ def test_prediction_logic():
         return datetime(2023, 1, 1, hour, minute, second)
 
     data = [
-        {'PULocationID': 1, 'DOLocationID': 2, 'tpep_pickup_datetime': dt(1, 1), 'tpep_dropoff_datetime': dt(1, 10)},
+        {
+            'PULocationID': 1,
+            'DOLocationID': 2,
+            'tpep_pickup_datetime': dt(1, 1),
+            'tpep_dropoff_datetime': dt(1, 10)
+        },
     ]
     df = pd.DataFrame(data)
 
     # Здесь должна быть ваша функция, которая делает предсказания
     # Например, `predictions = predict.make_predictions(df)`
-    
+
     # Симулируем результат
-    df['predicted_duration'] = 9.0 
-    
+    df['predicted_duration'] = 9.0
+
     assert 'predicted_duration' in df.columns
     assert df.iloc[0]['predicted_duration'] > 0
