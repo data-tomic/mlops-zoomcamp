cc156f6d8436848bc8036e5b8adbe198a52da3cc
Sergei Plekhov <sergei.plekhov@dgoi.ru>
2025-06-29 21:40:41 +0300

    Lint with flake recommendation

--- 06-best-practices/integration_test.py
+++ 06-best-practices/integration_test.py
@@ -3,12 +3,14 @@ import pandas as pd
 from datetime import datetime
 
 # Функция для создания тестовых дат
+
 def dt(hour, minute, second=0):
     return datetime(2023, 1, 1, hour, minute, second)
 
 # --- Читаем параметры из переменных окружения ---
 # Пайплайн передаст сюда значения из UI.
 # Если запускаем локально и переменные не заданы, используются значения по умолчанию.
+
 YEAR = int(os.getenv("YEAR", "2023"))
 MONTH = int(os.getenv("MONTH", "1"))
 
@@ -23,13 +25,13 @@ data = [
     (None, None, dt(1, 1), dt(1, 10)),
     (1, 1, dt(1, 2), dt(1, 10)),
     (1, None, dt(1, 2, 0), dt(1, 2, 59)),
-    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),   
 ]
 columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
 df_input = pd.DataFrame(data, columns=columns)
 
 # Имя бакета нужно указать явно
-S3_BUCKET_NAME = "mlops-zoomcamp-data-tomic-2025" # <-- УКАЖИТЕ ИМЯ ВАШЕГО БАКЕТА
+S3_BUCKET_NAME = "mlops-zoomcamp-data-tomic-2025"  # <-- УКАЖИТЕ ИМЯ ВАШЕГО БАКЕТА
 
 input_file = f"s3://{S3_BUCKET_NAME}/in/{YEAR:04d}-{MONTH:02d}.parquet"
 output_file = f"s3://{S3_BUCKET_NAME}/out/{YEAR:04d}-{MONTH:02d}.parquet"
@@ -56,7 +58,7 @@ sum_predicted_durations = df_result['predicted_duration'].sum()
 
 print("\n==========================================")
 print(f"RESULT: Sum of predicted durations = {sum_predicted_durations}")
-print(f"==========================================")
+print("==========================================")
 
 # Простая проверка, чтобы пайплайн мог "упасть", если результат неверный
 assert abs(sum_predicted_durations - 36.28) < 0.1, "Result is not as expected!"
